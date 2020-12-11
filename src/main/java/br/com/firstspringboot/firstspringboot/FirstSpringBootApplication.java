d381033d606e4a3293b9738071759324f5b105a6
paololeandro <dipaolosantos@gmail.com>
2020-12-11 19:36:15 -0300

    First commit

---
+++ src/main/java/br/com/firstspringboot/firstspringboot/FirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.firstspringboot.firstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApplication.class, args);
+	}
+
+}
